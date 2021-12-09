54d9cfa437730688a896e5c20458eb0d82c9791d
theoprovost <theo.provost@gmail.com>
2021-12-09 14:29:09 +0100

    Changed syntax to provide SSL support to PG

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 db_client = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    sslmode: process.env.NODE_ENV === "production" ? "require" : "disable"
 })
 
 module.exports = db_client
